a5451eb88b93c2825bfc97008e986c17208b82db
dakota-hawkins <dakotahawkins0110@gmail.com>
2018-11-26 15:30:13 -0500

    parameter space to ga.

--- tests/mendel_tests.py
+++ tests/mendel_tests.py
@@ -110,4 +110,4 @@ class TestGeneticAlgorithm:
                                      'exon2': 'Aa'},
                            'gene4': {'exon1': {'transcript1': 'GG',
                                                'transcript2': 'CC'}}}
-        self.ga = mendel.GeneticAlgorithm()
+        self.ga = mendel.GeneticAlgorithm(parameter_space)
